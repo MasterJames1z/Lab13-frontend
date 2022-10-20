563b84dd3c8f3858b881350a024f480ce53e0431
Worawut Aunkham <jameskater350@gmail.com>
2022-10-21 01:09:12 +0700

    4.13

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -17,10 +17,18 @@ export default {
       })
       .then((response) => {
         localStorage.setItem('token', response.data.token)
+        localStorage.setItem('user', JSON.stringify(response.data.user))
         return Promise.resolve(response.data)
       })
       .catch((error) => {
         return Promise.reject(error)
       })
+  },
+  logout() {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+  },
+  getUser() {
+    return JSON.parse(localStorage.getItem('user'))
   }
 }
